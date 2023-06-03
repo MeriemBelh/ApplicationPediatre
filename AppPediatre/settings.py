57fd9459afca47f7a332e9dd1704fccec25304bf
MeriemBelh <meriem.balhaddad2001@gmail.com>
2023-06-03 01:06:36 +0100

    database config

--- AppPediatre/settings.py
+++ AppPediatre/settings.py
@@ -101,16 +101,13 @@ DATABASES = {
         'USER': 'dbpediatre_user',
         'PASSWORD': '2d9PmVfcgX6pfwMHmO8Sa77fdKDb2vYM',
         'HOST': 'dpg-chruj0grddl7atfg00c0-a.oregon-postgres.render.com',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
-
 db_from_env = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 DATABASES['default'].update(db_from_env)
 
-db_from_env = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
